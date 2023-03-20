25c4440f3880d185e441240d065c5d9e7830f351
Andy Wang <v-wajian@microsoft.com>
2023-03-20 09:40:25 +0000

    Add project files.

---
+++ Mvc70Ind/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Mvc70Ind;
+
+namespace Mvc70Ind.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Mvc70Ind.Movie> Movie { get; set; } = default!;
+    }
+}
